90e09b1a8b2408ab67c542c9324dab27d908dba1
Irvin Capagcuan <irvin@capagcuan.org>
2021-05-08 19:46:54 +0800

    Added nvm

--- custom.zsh
+++ custom.zsh
@@ -21,3 +21,5 @@ export PATH=$PATH:$HOME/.local/bin:$HOME/bin:$HOME/.composer/vendor/bin:/usr/loc
 
 export SSH_KEY=$(cat ~/.ssh/id_rsa) 
 export PUPPETEER_EXECUTABLE_PATH=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
